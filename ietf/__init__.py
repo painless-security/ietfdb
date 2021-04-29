8716a3d842ddf1916b583b6c118624783afca95d
Mark Donnelly <mark@painless-security.com>
2021-04-29 16:32:37 -0400

    7.28.1.dev0

---
+++ ietf/__init__.py
@@ -0,0 +1,17 @@
+# Copyright The IETF Trust 2007-2020, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+
+from . import checks                           # pyflakes:ignore
+
+# Don't add patch number here:
+__version__ = "7.28.1.dev0"
+
+# set this to ".p1", ".p2", etc. after patching
+__patch__   = ""
+
+__date__    = "$Date: 2021-04-15 10:54:15 -0400 (Thu, 15 Apr 2021) $"
+
+__rev__     = "$Rev: 18956 $ (dev) Latest release: Rev. 18954 "
+
+__id__      = "$Id: __init__.py 18956 2021-04-15 14:54:15Z rjsparks@nostrum.com $"
