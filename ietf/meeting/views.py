b5bfa9ec875c20c00978af4c076b7f0f45237a01
Jennifer Richards <jennifer@painless-security.com>
2021-07-30 16:55:27 -0300

    Fix failing test and a merge error
      * Secretariat no longer gets filter buttosn, so create a special group for
        testing individual agenda session selection
      * Add an import dropped during merge

--- ietf/meeting/views.py
+++ ietf/meeting/views.py
@@ -65,7 +65,8 @@ from ietf.meeting.helpers import get_wg_list, find_ads_for_meeting
 from ietf.meeting.helpers import get_meeting, get_ietf_meeting, get_current_ietf_meeting_num
 from ietf.meeting.helpers import get_schedule, schedule_permissions
 from ietf.meeting.helpers import preprocess_assignments_for_agenda, read_agenda_file
-from ietf.meeting.helpers import filter_keywords_for_session, tag_assignments_with_filter_keywords, filter_keyword_for_specific_session
+from ietf.meeting.helpers import filter_keywords_for_session, tag_assignments_with_filter_keywords
+from ietf.meeting.helpers import filter_keyword_for_specific_session, is_regular_agenda_filter_group
 from ietf.meeting.helpers import convert_draft_to_pdf, get_earliest_session_date, AgendaFilterOrganizer
 from ietf.meeting.helpers import can_view_interim_request, can_approve_interim_request
 from ietf.meeting.helpers import can_edit_interim_request
