e207dc21175254250eb11be76915f0d8f4749f7c
Jennifer Richards <jennifer@painless-security.com>
2021-08-20 12:47:41 -0300

    Fix test_proceedings_no_agenda when not running all tests
    This is the same as the fix for ticket 3385

--- ietf/meeting/views.py
+++ ietf/meeting/views.py
@@ -3796,8 +3796,9 @@ def proceedings(request, num=None):
 
     meeting = get_meeting(num)
 
+    import pdb; pdb.set_trace()
     if (meeting.number.isdigit() and int(meeting.number) <= 96):
-        return HttpResponseRedirect( 'https://www.ietf.org/proceedings/%s' % num )
+        return HttpResponseRedirect('https://www.ietf.org/proceedings/%s' % meeting.number)
 
     if not meeting.schedule or not meeting.schedule.assignments.exists():
         kwargs = dict()
