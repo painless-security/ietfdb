64ad84357b19c318458663887448ad2c7fc1c427
Jennifer Richards <jennifer@painless-security.com>
2022-01-26 21:34:17 -0400

    Do not render template paths when in production mode

--- ietf/utils/templatetags/origin.py
+++ ietf/utils/templatetags/origin.py
@@ -1,27 +1,40 @@
+# Copyright The IETF Trust 2015-2022, All Rights Reserved
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
 from django import template
+from django.conf import settings
+
 import debug                            # pyflakes:ignore
+from ietf.utils import log
 
 register = template.Library()
 
 
 class OriginNode(template.Node):
-    def __init__(self, origin=None):
+    def __init__(self, origin_=None):
         # template file path if the template comes from a file:
-        self.origin = origin
+        self.origin = origin_
+
+    def relative_path(self):
+        origin_path = Path(str(self.origin))
+        try:
+            return origin_path.relative_to(settings.BASE_DIR)
+        except ValueError:
+            log.log(f'Rendering a template from outside the project root: {self.origin}')
+            return '** path outside project root **'
 
     def render(self, context):
-        if self.origin:
-            return "<!-- template: %s -->" % self.origin
+        if self.origin and settings.SERVER_MODE != 'production':
+            return f'<!-- template: {self.relative_path()} -->'
         else:
             return ""
 
 @register.tag
 def origin(parser, token):
-    """
-    Returns a node which renders the 
-    """
+    """Create a node indicating the path to the current template"""
     if hasattr(token, "source"):
         origin, source = token.source
-        return OriginNode(origin=origin)
+        return OriginNode(origin_=origin)
     else:
         return OriginNode()
